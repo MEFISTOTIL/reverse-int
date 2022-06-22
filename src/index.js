b9410da7ee60ae8035140e7e7622607992c8cff2
MEFISTOTIL <s.e.m.e.n.o.v.k.v.a.v@gmail.com>
2022-06-22 11:38:06 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+// module.exports = function reverse (n) {
+
+// }
+module.exports = function reverse(n) {
+	if (n >= 0) {
+		n = n + ""
+		return n.split("").reverse().join("");
+	}
+	else {
+		n = n * (-1) + ""
+		return n.split("").reverse().join("");
+	}
 }
